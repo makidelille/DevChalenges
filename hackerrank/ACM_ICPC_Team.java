ba78bb6f13a4550501a9e3af9e480e8b2a836ffd
makidelille <makidelille@github.com>
2015-05-04 10:45:35 +0200

    ACM ICPC Team done

---
+++ hackerrank/ACM_ICPC_Team.java
@@ -0,0 +1,38 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ACM_ICPC_Team {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i=0; i < n; i++){
+			String line = sc.next();
+			for(int j=0; j< m; j++)
+				arr[i][j] = Integer.parseInt(Character.toString(line.charAt(j)));
+		}
+		//first task
+		int max = 0;
+		List<Integer> l = new ArrayList<>();
+		for(int i=0; i <n; i++){
+			int[] p1 = arr[i];
+			for(int j=i+1; j< n; j++){
+				int[] p2 = arr[j];
+				int topic = 0;
+				for(int k=0; k < m; k++) if((p1[k] | p2[k]) == 1) topic++;
+				if(topic > max) max = topic;
+				l.add((Integer)topic);
+			}
+		}
+		System.out.println(max);
+		System.out.println(Collections.frequency(l, (Integer)max));
+		
+	}
+
+}
