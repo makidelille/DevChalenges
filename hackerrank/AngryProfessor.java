18849e69ad1564e0e2f977847b0064ab5199c6a0
makidelille <makidelille@github.com>
2015-05-04 09:46:11 +0200

    angry professor done

---
+++ hackerrank/AngryProfessor.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class AngryProfessor {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		for(int a = 0; a < t; a++){
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int[] arr = new int[n];
+			int missing = 0;
+			for (int i=0; i< n; i++){
+				arr[i] = sc.nextInt();
+				if(arr[i] > 0) 
+					missing++;
+			}
+			System.out.println(n- missing < k ? "YES" : "NO");
+		}
+	}
+
+}
