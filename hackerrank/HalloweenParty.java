caf6f037c01878d6cfa5cdedf3dfa210ba618100
makidelille <makidelille@github.com>
2015-05-04 09:52:48 +0200

    Halloween party done

---
+++ hackerrank/HalloweenParty.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class HalloweenParty {
+
+	public static void main(String[] args) {
+		//already done this one ! :D
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int a=0; a < t;a++){
+			System.out.println((int)Math.floor(Math.pow(sc.nextInt()/2f, 2)));
+		}
+		
+		
+		
+	}
+
+}
