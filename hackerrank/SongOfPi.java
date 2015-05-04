411af495d9f34eba0b70001b43afa2fb56a507c4
makidelille <makidelille@github.com>
2015-05-04 15:42:44 +0200

    Pi song done

---
+++ hackerrank/SongOfPi.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class SongOfPi {
+
+	public static void main(String[] args) {
+		final String PI = "31415926535897932384626433833";
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		for(int a=0; a < t; a++){
+			String s = sc.nextLine();
+			String[] split = s.split(" ");
+			StringBuilder buil = new StringBuilder();
+			for(int i=0; i< split.length;i++){
+				 buil.append(split[i].replaceAll(" ", "").length());
+			}
+			System.out.println(PI.startsWith(buil.toString()) ? "It's a pi song." : "It's not a pi song.");
+		}
+	}
+
+}
