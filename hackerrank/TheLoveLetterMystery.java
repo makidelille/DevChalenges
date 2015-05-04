393a96825712ca6d3fcc55dea92f8cde8d29b536
makidelille <makidelille@github.com>
2015-05-04 09:05:37 +0200

    love letter mystery completed

---
+++ hackerrank/TheLoveLetterMystery.java
@@ -0,0 +1,33 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class TheLoveLetterMystery {
+
+	// 'a' = 97
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		
+		for (int a = 0; a < t; a++) {
+			int operations = 0;
+			String s = sc.next();
+			int[] arr = new int[s.length()];
+			for (int i = 0; i < s.length(); i++) {
+				arr[i] = s.charAt(i);
+			}
+			// logic
+			for (int j = 0; j < arr.length / 2; j++) {
+				int first = arr[j];
+				int last = arr[arr.length - j-1];
+				int diff = Math.abs((first - last));
+				operations += diff;
+			}
+			System.out.println(operations);
+
+		}
+	}
+
+}
