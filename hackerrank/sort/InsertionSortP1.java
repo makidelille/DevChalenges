1a5fd24ac336d9bffcc4d11cc48768d83aa8e6de
makidelille <makidelille@github.com>
2015-05-06 20:10:43 +0200

    insertion sort p1 done

---
+++ hackerrank/sort/InsertionSortP1.java
@@ -0,0 +1,28 @@
+package hackerrank.sort;
+
+import java.util.Scanner;
+
+public class InsertionSortP1 {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int s = sc.nextInt();
+	int[] arr = new int[s];
+	for(int i=0; i< s; i++) arr[i] = sc.nextInt();
+	int toSort = arr[s-1];
+	for(int j = s-2; j >= 0 ; j--){
+	    boolean found = true;
+	    if(toSort < arr[j]){
+		arr[j+1] = arr[j];
+		found = false;
+	    }else{
+		arr[j+1] = toSort;
+	    } 
+	    for(int k=0; k < s; k++) System.out.print(arr[k] + " ");
+	    System.out.print("\n");
+	    if(found) break;
+	    if(!found && j==0) arr[0] = toSort;
+	}
+    }
+
+}
