b985d6a8fe3d3f8a68594e4d3bc6d24b501e8e8a
makidelille <makidelille@github.com>
2015-05-06 19:40:06 +0200

    tut done

---
+++ hackerrank/sort/TutIntro.java
@@ -0,0 +1,16 @@
+package hackerrank.sort;
+
+import java.util.Scanner;
+
+
+public class TutIntro {
+
+    public static void main(String[] args) {
+	Scanner sc  = new Scanner(System.in);
+	int v  = sc.nextInt();
+	int n = sc.nextInt();
+	for(int i=0; i< n; i++) if(v == sc.nextInt()) System.out.println(i);
+	
+    }
+
+}
